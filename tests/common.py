b3ce498a94ffc9cc19c626e6d4aff229b56600d5
Ariana Eisenstein <ariana.eisenstein@gmail.com>
2020-03-19 00:38:56 -0400

    types: add literal support for python 3.7
    Add literal support for python 3.7 by importing the Literal from
    typing_extensions for python3.7.

--- tests/common.py
+++ tests/common.py
@@ -2,4 +2,5 @@ import sys
 
 import pytest
 
-literal_support = init_var_type_support = pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python 3.8")
+literal_support = pytest.mark.skipif(sys.version_info < (3, 7), reason="requires Python 3.7")
+init_var_type_support = pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python 3.8")
